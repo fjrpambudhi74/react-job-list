0bdea85f11849f1c21fddbd3c0458a97c2f6c1d0
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2023-01-31 12:29:03 +0700

    create input search component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleLogout } from "react-google-login";
 
 const Dashboard = ({setLogin}) => {
+
+  const [search, setSearch] = useState({
+    description: '',
+    location: '',
+    fullTime: false
+  })
+
+  const handleInput = (event) => {
+    const {name, value} = event.target
+    if(name === 'fullTime') {
+      setSearch((prevState) => ({...search, [name] : !prevState.fullTime}))
+    } else {
+      setSearch({...search, [name] : value})
+    }
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    console.log(search)
+  }
+
   const clientId =
     "438375057615-86gf8du179pahebqq8jelnj06hcl1i4g.apps.googleusercontent.com";
 
@@ -11,6 +32,27 @@ const Dashboard = ({setLogin}) => {
   return (
     <>
       <h1>Dashboard</h1>
+      <input
+        type="text"
+        name="description"
+        placeholder="Description"
+        value={search.description}
+        onChange={handleInput}
+      />
+      <input
+        type="text"
+        name="location"
+        placeholder="Location"
+        value={search.location}
+        onChange={handleInput}
+      />
+      <input
+        type="checkbox"
+        name="fullTime"
+        checked={search.fullTime}
+        onChange={handleInput}
+      />
+      <button onClick={handleSearch}>Search</button>
       <GoogleLogout
         clientId={clientId}
         buttonText="Log out"
