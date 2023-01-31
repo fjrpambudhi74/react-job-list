e37a0c6b6f83f59475dada9bd7c6d318cdf0f7a2
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2023-01-31 11:59:58 +0700

    set logout account

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { GoogleLogout } from "react-google-login";
 
-const Dashboard = () => {
+const Dashboard = ({setLogin}) => {
+  const clientId =
+    "438375057615-86gf8du179pahebqq8jelnj06hcl1i4g.apps.googleusercontent.com";
+
+  const logOut = () => {
+    setLogin(null);
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <h1>Dashboard</h1>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Log out"
+        onLogoutSuccess={logOut}
+      />
+    </>
+  );
 }
 
 export default Dashboard
