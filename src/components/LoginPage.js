790c4d31083a53b411cdd99ea8cd4932a836d5cf
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2023-01-31 11:06:22 +0700

    implement google login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
 
 const LoginPage = () => {
+  const clientId =
+    '438375057615-86gf8du179pahebqq8jelnj06hcl1i4g.apps.googleusercontent.com';
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
+
+  const onSuccess = (res) => {
+    console.log("success:", res);
+  };
+  const onFailure = (err) => {
+    console.log("failed:", err);
+  };
+
   return (
-    <div>Login Page</div>
-  )
+    <>
+      <h1>Login Page</h1>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Sign in with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+    </>
+  );
 }
 
 export default LoginPage
